964d94295d5a489eef43b82e57a4186fae72d03d
Sytoplis <Sytoplis@gmx.de>
2022-08-26 00:09:51 +0200

    tiles

--- Assets/Scripts/Enemies/Spawn.cs
+++ Assets/Scripts/Enemies/Spawn.cs
@@ -13,12 +13,14 @@ public class Spawn : MonoBehaviour
 
     public IEnumerator SpawnOj(float delay) {
         yield return new WaitForSeconds(delay);
-        Vector3 pos = transform.position + spawnArea.GetRndm();
+        //Vector3 pos = transform.localToWorldMatrix * spawnArea.GetRndm();
+        Vector3 pos = transform.TransformPoint(spawnArea.GetRndm());
         Instantiate(prefab, pos, Quaternion.identity, PollingStation.Instance.gameManager.GetEntityParent());
     }
 
     private void OnDrawGizmosSelected() {
         Gizmos.color = new Color(0.1f, 0.6f, 0.2f);
-        Gizmos.DrawWireCube(transform.position + spawnArea.center, spawnArea.size);
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(spawnArea.center, spawnArea.size);
     }
 }
